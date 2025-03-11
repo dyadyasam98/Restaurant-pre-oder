80db051ae3348669591c46439a884571e480bb9e
dyadyasam98 <strogaya.tt@mail.ru>
2025-03-11 21:28:02 +0700

    file_split

--- main.py
+++ main.py
@@ -1,66 +1,4 @@
-class Menu:
-    def __init__(self, item_id, name, price):
-        self.item_id = item_id
-        self.name = name
-        self.price = price
-
-class Table:
-    def __init__(self, table_number, seats, if_booked):
-        self.table_number = table_number
-        self.seats = seats
-        self.if_booked = if_booked
-
-    def book(self):
-        if self.if_booked == False:
-            self.if_booked = True
-            return True
-        return False
-
-class Order:
-    def __init__(self, table, time):
-        self.table = table
-        self.time = time
-        self.items = []
-
-    def add_item(self, menu_item):
-        self.items.append(menu_item)
-
-class Restaurant:
-    def __init__(self, name):
-        self.name = name
-        self.tables = []
-        self.menu = []
-        self.orders = []
-
-    def add_table(self, table):
-        self.tables.append(table)
-
-    def add_menu_item(self, menu_item):
-        self.menu.append(menu_item)
-
-    def get_available_tables(self):
-        available_tables = []
-        for table in self.tables:
-            if table.if_booked == False:
-                available_tables.append(table)
-        return available_tables
-
-    def book_table(self, table_number, time):
-        for table in self.tables:
-            if table.table_number == table_number and table.book() == True:
-                order = Order(table, time)
-                self.orders.append(order)
-                return order
-        return None
-
-    def place_order(self, table_number, menu_items):
-        for order in self.orders:
-            if order.table.table_number == table_number:
-                for item in menu_items:
-                    order.add_item(item)
-                return order
-        return None
-
+from main_entities import Restaurant, Table, Order, Menu
 restaurant = Restaurant("Ресторан 1")
 restaurant.add_table(Table(1, 2, False))
 restaurant.add_table(Table(2, 4, False))
@@ -94,6 +32,7 @@ if order:
         dish_name = input("Введите название блюда для предзаказа (или 'завершить' для завершения): ")
         if dish_name.lower() == 'завершить':
             break
+        dish_quantity = int(input("Введите количество: "))
 
         # ищем блюдо
         user_dish = 0
@@ -130,7 +69,7 @@ if order:
                 break
         # Если не нашли блюдо в заказе, то создаем новое с количесвтом 1
         if dish_status == False:
-            order_summary.append({'name': item.name, 'quantity': 1, 'price': item.price})
+            order_summary.append({'name': item.name, 'quantity': dish_quantity, 'price': item.price})
 
         total_price += item.price
     # Выводим заказ для юзера
---
+++ main_entities.py
@@ -0,0 +1,62 @@
+class Menu:
+    def __init__(self, item_id, name, price):
+        self.item_id = item_id
+        self.name = name
+        self.price = price
+
+class Table:
+    def __init__(self, table_number, seats, if_booked):
+        self.table_number = table_number
+        self.seats = seats
+        self.if_booked = if_booked
+
+    def book(self):
+        if self.if_booked == False:
+            self.if_booked = True
+            return True
+        return False
+
+class Order:
+    def __init__(self, table, time):
+        self.table = table
+        self.time = time
+        self.items = []
+
+    def add_item(self, menu_item):
+        self.items.append(menu_item)
+
+class Restaurant:
+    def __init__(self, name):
+        self.name = name
+        self.tables = []
+        self.menu = []
+        self.orders = []
+
+    def add_table(self, table):
+        self.tables.append(table)
+
+    def add_menu_item(self, menu_item):
+        self.menu.append(menu_item)
+
+    def get_available_tables(self):
+        available_tables = []
+        for table in self.tables:
+            if table.if_booked == False:
+                available_tables.append(table)
+        return available_tables
+
+    def book_table(self, table_number, time):
+        for table in self.tables:
+            if table.table_number == table_number and table.book() == True:
+                order = Order(table, time)
+                self.orders.append(order)
+                return order
+        return None
+
+    def place_order(self, table_number, menu_items):
+        for order in self.orders:
+            if order.table.table_number == table_number:
+                for item in menu_items:
+                    order.add_item(item)
+                return order
+        return None
